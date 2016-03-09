325280e8b95dd878bb12ccc44758f6235d2105ad
Conlin Durbin <conlindurbin@gmail.com>
2016-03-09 11:55:29 -0500

    Fixing undefined bug on release generator

--- release-generator/app.js
+++ release-generator/app.js
@@ -11,8 +11,11 @@ function getWords(data) {
   return data.json().then(function(words) {
     var wL = words.map(function(w) {return capitalize(w.word)}).filter(function(word) {
       var noun = document.querySelector(".endWord").value;
-      return word.charAt(0) === noun.charAt(0).toUpperCase()
+      return word.charAt(0) === noun.charAt(0).toUpperCase();
     });
+    if(wL.length === 0) {
+      return getAdjective();
+    }
     return wL;
   });
 } 
@@ -23,7 +26,7 @@ function selectRandom(words) {
 }
 
 function getAdjective() {
-  return fetch("http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=false&includePartOfSpeech=adjective&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&sortBy=alpha&sortOrder=asc&limit=200&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5").then(getWords).then(selectRandom);
+  return fetch("http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=false&includePartOfSpeech=adjective&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&sortBy=alpha&sortOrder=asc&limit=300&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5").then(getWords).then(selectRandom);
 }
 
 Go.addEventListener('click', function() {
