99c2b62cf9bad037d0a7cca40f066cb346c96cd9
Conlin Durbin <conlindurbin@gmail.com>
2016-03-09 12:17:30 -0500

    Adding tweet button and fixing some bugs to Release Generator

--- release-generator/app.js
+++ release-generator/app.js
@@ -19,19 +19,39 @@ function getWords(data) {
 
 function selectRandom(words) {
   var randIndex = Math.floor(Math.random() * (words.length));
-  adj = words[randIndex];
+  var adj = words[randIndex];
   if(!adj) {
     return getAdjective();
   }
+  return adj;
 }
 
 function getAdjective() {
   return fetch("http://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=false&includePartOfSpeech=adjective&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&sortBy=alpha&sortOrder=asc&limit=300&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5").then(getWords).then(selectRandom);
 }
 
+function GenerateTwitterButton(text) {
+  var link = document.createElement("a");
+  link.setAttribute("class", "twitter-share-button");
+  var tweet = encodeURIComponent("My next release name: "+text+"! Find yours @ http://conlin.xyz/release-generator, made by @ohmfox.");
+  link.setAttribute("href", "https://twitter.com/intent/tweet?text="+tweet);
+  link.innerText = "Share on Twitter";
+  return link;
+}
+
+function GenerateReleaseDiv(adj, noun) {
+  var twitter = GenerateTwitterButton(adj+" "+noun);
+  var text = document.createElement("h1");
+  text.innerText = adj+" "+noun;
+  console.log(text, twitter);
+  Release.appendChild(text);
+  Release.appendChild(twitter);
+}
+
 Go.addEventListener('click', function() {
   getAdjective().then(function(adj) {
     var noun = document.querySelector(".endWord").value;
-    Release.textContent = adj +" "+ noun;
+    Release.innerHTML = "";
+    GenerateReleaseDiv(adj, noun);
   });
 })
