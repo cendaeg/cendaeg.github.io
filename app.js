367fe1c077ef6c2a87ce88a4a16c170720eb0a17
Conlin Durbin <conlindurbin@gmail.com>
2016-08-15 15:41:33 -0400

    Adding picture thing and dark background

--- app.js
+++ app.js
@@ -90,3 +90,52 @@ function getRecentPost() {
 
 addToPage("#music", getRecentMusic());
 addToPage("#post", getRecentPost());
+
+'use strict';
+var cvss = document.querySelectorAll('.glitch_port');
+var _loop = function _loop() {
+  var cvs = cvss[i];
+  var ctx = cvs.getContext('2d');
+  var imgObj = new Image();
+  var tiles = [];
+  var globalX = 150;
+  var globalY = 150;
+  var tile = function tile(img, sX, sY, sHeight, sWidth) {
+    var neg = Math.random() > 0.5 ? -1 : 1;
+    var randomShift = (Math.random() * 4 - 1) * neg * Math.random();
+    var moveScale = 0.25;
+    return {
+      draw: function draw() {
+        ctx.drawImage(imgObj, sX, sY, sHeight, sWidth, window.innerWidth / 2 + sX + globalX * moveScale * randomShift * moveScale, -75 + sY + globalY * moveScale * randomShift * moveScale, sHeight, sWidth);
+      }
+    };
+  };
+  var redrawTiles = function redrawTiles() {
+    for (var _i = 0; _i < tiles.length; _i++) {
+      tiles[_i].draw();
+    }
+  };
+  imgObj.onload = function () {
+    cvs.height = window.innerHeight;
+    cvs.width = window.innerWidth;
+    for (var i = 0; i < 300; i++) {
+      var sX = imgObj.width / (2 * Math.random() + 1);
+      var sY = 100 + imgObj.height * Math.random();
+      var sHeight = 100 * Math.random();
+      var sWidth = 100 * Math.random();
+      var t = new tile(imgObj, sX, sY, sHeight, sWidth);
+      tiles.push(t);
+    }
+    redrawTiles();
+  };
+  window.addEventListener('mousemove', function (e) {
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    globalX = e.clientX - 300;
+    globalY = e.clientY - 300;
+    redrawTiles();
+  });
+  imgObj.src = cvs.dataset.src;
+};
+for (var i = 0; i < cvss.length; i++) {
+  _loop();
+}
